6fd07563fbfee93a0ef5b540473aba31cea0865b
Jude Zhang <564193687@qq.com>
2019-04-14 18:57:54 +0800

    change $home in .fzf.bash

--- .fzf.bash
+++ .fzf.bash
@@ -1,13 +1,13 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/zhajio/.fzf/bin* ]]; then
-  export PATH="${PATH:+${PATH}:}/home/zhajio/.fzf/bin"
+if [[ ! "$PATH" == *$HOME/.fzf/bin* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/home/zhajio/.fzf/shell/completion.bash" 2> /dev/null
+[[ $- == *i* ]] && source "$HOME/.fzf/shell/completion.bash" 2> /dev/null
 
 # Key bindings
 # ------------
-source "/home/zhajio/.fzf/shell/key-bindings.bash"
+source "$HOME/.fzf/shell/key-bindings.bash"
